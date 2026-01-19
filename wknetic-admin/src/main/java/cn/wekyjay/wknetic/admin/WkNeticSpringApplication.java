13bfb6f0838213c1a25647c310dafa12b47c94a7
WekyJay <505083907@qq.com>
2026-01-19 23:31:19 +0800

    feat: initial project structure with Java 21 and Spring Boot 3.5.9

---
+++ wknetic-admin/src/main/java/cn/wekyjay/wknetic/admin/WkNeticSpringApplication.java
@@ -0,0 +1,12 @@
+package cn.wekyjay.wknetic.admin;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkNeticSpringApplication {
+    public static void main(String[] args) {
+
+    }
+
+}
